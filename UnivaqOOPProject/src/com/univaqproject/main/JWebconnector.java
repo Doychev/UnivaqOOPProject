82d39f4e025c3a183fc6c7c2b743dd5b0081bc87
Doychev <martin.doychev@abv.bg>
2015-07-13 21:46:52 +0200

    web connector fix

--- UnivaqOOPProject/src/com/univaqproject/main/JWebconnector.java
+++ UnivaqOOPProject/src/com/univaqproject/main/JWebconnector.java
@@ -1,35 +1,42 @@
 package com.univaqproject.main;
 
 import com.jaunt.Element;
+import com.jaunt.Elements;
 import com.jaunt.JauntException;
 import com.jaunt.UserAgent;
 
 public class JWebconnector 
 {
-	public static void function1 (String[]Args)
+	public static void main (String[]Args)
 	{
 		try
 		{
 			
 		UserAgent userAgent = new UserAgent();         //create new userAgent (headless browser)
-		userAgent.visit("http://blog.bg");          //visit link
+		userAgent.visit("http://facebook.com");          //visit link
 		
-		Element sitems = userAgent.doc.findEvery("<div>").findEvery("<a>");  //find search result links
-		System.out.println(sitems.getAt("href"));           //print results
+//		Element sitems = userAgent.doc.findEvery("<div>").findEvery("<a>");  //find search result links
+//		System.out.println(sitems.getAt("href"));           //print results
 		
-		sitems = userAgent.doc.findFirst("<body>"); //find every text in body
-		System.out.println(sitems.getText());
+		StringBuilder sb = new StringBuilder();
+		Elements sitems = userAgent.doc.findEvery("<div>"); //find every text in body
+		for (Element sitem : sitems) {
+			sb.append(sitem.getText());
+//			System.out.print(printing);			
+		}
+		System.out.println(sb.toString().replaceAll("\n", "").replaceAll("\t", "").replaceAll("\r", ""));			
 		
-		String title = userAgent.doc.findFirst("<title>").getText(); //get child text of title element.
-		System.out.println(title); 
 		
-		Element head = userAgent.doc.findFirst("<head>");  //find search result links		
-		System.out.println(head.getText());           //print results
+//		String title = userAgent.doc.findFirst("<title>").getText(); //get child text of title element.
+//		System.out.println(title); 
+//		
+//		Element head = userAgent.doc.findFirst("<head>");  //find search result links		
+//		System.out.println(head.getText());           //print results
 		
-		//Element bodytxt = userAgent.doc.findFirst("<body>"); //find body and get text
-		//Element divtxt = bodytxt.findEvery("<div>");			//find div and get text
-		//System.out.pruintln(bodytxt.getText());
-		//System.out.println(divtxt.getText());
+//		Element bodytxt = userAgent.doc.findFirst("<body>"); //find body and get text
+//		Element divtxt = bodytxt.findEvery("<div>");			//find div and get text
+//		System.out.println(bodytxt.getText());
+//		System.out.println(divtxt.getText());
 		
 		}
 		catch(JauntException e)
